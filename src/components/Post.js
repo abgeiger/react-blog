4a376586e8a329a8f2a3afd50085f0384f4cdd0b
abgeiger <30698092+abgeiger@users.noreply.github.com>
2020-03-20 14:28:15 -0500

    Hard code example posts.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 
+const blogPosts = [
+    {
+        id: 1,
+        title: 'Post 1',
+        content: 'Content of the first post.',
+    },
+    {
+        id: 2,
+        title: 'Post 2',
+        content: 'Content of the second post.',
+    },
+    {
+        id: 3,
+        title: 'Post 3',
+        content: 'Content of the third post.',
+    },
+];
+
 export class Post extends Component {
     render() {
         return (
             <div>
-                <h3>Post Title</h3>
-                <p>Post content.</p>
+                <h3>{blogPosts[1].title}</h3>
+                <p>{blogPosts[1].content}</p>
             </div>
         )
     }
