ca72011a5dd1655b4914120a7c902060d518b1ab
abgeiger <30698092+abgeiger@users.noreply.github.com>
2020-03-20 13:38:13 -0500

    Connect components to App.js.

---
+++ src/components/Post.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+export class Post extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Post Title</h3>
+                <p>Post content.</p>
+            </div>
+        )
+    }
+}
+
+export default Post
---
+++ src/components/PostList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Post from './Post';
+// import PropTypes from 'prop-types';
+
+export class PostList extends Component {
+    render() {
+        return (
+            <Post />
+        )
+    }
+}
+
+export default PostList
