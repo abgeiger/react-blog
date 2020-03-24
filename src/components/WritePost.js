7827f1aa297370a08dab4c29ea893a15024330ab
abgeiger <30698092+abgeiger@users.noreply.github.com>
2020-03-24 10:51:01 -0500

    Require a new post to have a title AND content

--- src/components/WritePost.js
+++ src/components/WritePost.js
@@ -2,27 +2,32 @@ import React, { Component } from 'react'
 // import PropTypes from 'prop-types';
 
 export class WritePost extends Component {
+    // The title and content for the new post
     state = {
         title: '',
         content: '',
     }
 
+    // Handles changes to the new post's title and content
     onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
+    // Sumbmits the new post to the Post List
     onSubmit = (e) => {
         e.preventDefault();
+        // If statement requires there to be a title and content in order to submit new post
+        if (this.state.title !== '' && this.state.content !== '') {
+            const newPost = {
+                title: this.state.title,
+                content: this.state.content,
+            }
 
-        const newPost = {
-            title: this.state.title,
-            content: this.state.content,
+            this.props.createPost(newPost);
+
+            this.setState({
+                title: '',
+                content: '',
+            });
         }
-
-        this.props.createPost(newPost);
-
-        this.setState({
-            title: '',
-            content: '',
-        });
     }
 
     render() {
